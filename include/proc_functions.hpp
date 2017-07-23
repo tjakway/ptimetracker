c55f0901f51b0f6ba3def90de5057ff0dc051ec6
Thomas Jakway <tjakway@nyu.edu>
2017-07-23 11:51:38 -0400

    set c++ standard -> C++11 began writing proc functions

--- include/proc_functions.hpp
+++ include/proc_functions.hpp
@@ -1,6 +1,20 @@
 #ifndef PROC_FUNCTIONS_HPP
 #define PROC_FUNCTIONS_HPP
 
+#include <regex>
+#include <string>
+
+class ProcMatcher
+{
+    const std::regex cwdRegex, procRegex;
+    const bool matchOnlyProgName;
+
+public:
+    ProcMatcher(std::regex cwdRegex, std::regex procRegex, bool matchOnlyProgName)
+        : cwdRegex(cwdRegex), procRegex(procRegex), matchOnlyProgName(matchOnlyProgName)
+        {}
+};
+
 
 
 #endif
---
+++ src/proc_functions.cpp
@@ -0,0 +1,1 @@
+#include "proc_functions.hpp"
