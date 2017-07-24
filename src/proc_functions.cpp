cb877ee3d6b9218620467df6c9ee8980cc4268ec
Thomas Jakway <tjakway@nyu.edu>
2017-07-24 13:40:20 -0400

    wrote ProcInfo functions

--- src/proc_functions.cpp
+++ src/proc_functions.cpp
@@ -2,11 +2,12 @@
 
 #include <vector>
 #include <string>
-#include <sstring>
-#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
 
 namespace {
-    std::string readFile(std::string filename)
+    std::string readFile(std::string inFileName)
     {
         //see https://stackoverflow.com/questions/2602013/read-whole-ascii-file-into-c-stdstring
 
@@ -17,21 +18,31 @@ namespace {
         std::stringstream strStream;
         strStream << inFile.rdbuf();//read the file
         return strStream.str();//str holds the content of the file
+        //destructor will close the file
     }
 
     class ProcInfo 
     {
         static std::string readCmdLine(pid_t pid)
         {
-
+            return readFile("/proc/" + std::to_string(pid) + "/cmdline");
         }
         static std::string readProcName(pid_t pid)
         {
-
+            return readFile("/proc/" + std::to_string(pid) + "/comm");
         }
         static std::string readCwd(pid_t pid)
         {
+            //use realpath to read the destination of the symlink as an absolute path
+            std::string cwdPath = "/proc/" + std::to_string(pid) + "/cwd";
 
+            char* allocedPath = realpath(cwdPath.c_str(), nullptr);
+
+            std::string absCwd = allocedPath;
+
+            //realpath will allocate a c string when the 2nd arg is NULL
+            free(allocedPath);
+            return absCwd;
         }
     };
 }
