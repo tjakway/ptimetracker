629788786e5851ac24fa2dfd9587952504245e38
Thomas Jakway <tjakway@nyu.edu>
2017-07-27 18:35:43 -0400

    fleshed out basic ProgramLoggerM

---
+++ src/TimeTracker/Types.hs
@@ -0,0 +1,39 @@
+module TimeTracker.Types where
+
+import TimeTracker.FFI
+import Foreign.Ptr (nullPtr)
+import Control.Monad.State
+
+data ProgramLoggerS = ProgramLoggerS {
+                    eventCallbacks :: [EventCallbackFunPtr],
+                    errorCallbacks :: [ErrorCallbackFunPtr],
+                    stopListeningCallbacks :: [StopListeningCallback],
+                    ioF :: IO (),
+                    apiState :: APIStatePtr
+                    }
+
+emptyProgramLoggerS = ProgramLoggerS {
+    eventCallbacks = [],
+    errorCallbacks = [],
+    stopListeningCallbacks = [],
+    ioF = return (),
+    apiState = nullPtr
+}
+
+type ProgramLoggerM a = State ProgramLoggerS a
+
+cleanupProgramLogger :: ProgramLoggerS -> IO ()
+cleanupProgramLogger s = do
+        freeAPIState (apiState s)
+        mapM_ . mapM_ freeHaskellFunPtr 
+            [(eventCallbacks s), (errorCallbacks s), (stopListeningCallbacks s)]
+
+runProgramLogger :: ProgramLoggerM a -> IO a
+runProgramLogger stateAction = do
+        apiStatePtr <- initializeAPIState
+
+        let start = emptyProgramLoggerS { apiState = apiStatePtr }
+            (res, finalState) = runState stateAction start
+
+        cleanupProgramLogger finalState
+        return res
