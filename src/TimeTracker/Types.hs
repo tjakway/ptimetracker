7f439c35a40fabb1da166696e9f8e5ccc2c860bf
Thomas Jakway <tjakway@nyu.edu>
2017-07-28 13:50:04 -0400

    modeled on LlvmM

--- src/TimeTracker/Types.hs
+++ src/TimeTracker/Types.hs
@@ -2,13 +2,11 @@ module TimeTracker.Types where
 
 import TimeTracker.FFI
 import Foreign.Ptr (freeHaskellFunPtr, nullPtr)
-import Control.Monad.State
 
 data ProgramLoggerS = ProgramLoggerS {
                     eventCallbacks :: [EventCallbackFunPtr],
                     errorCallbacks :: [ErrorCallbackFunPtr],
                     stopListeningCallbacks :: [StopListeningCallback],
-                    ioF :: IO (),
                     apiState :: APIStatePtr
                     }
 
@@ -17,20 +15,49 @@ emptyProgramLoggerS = ProgramLoggerS {
     eventCallbacks = [],
     errorCallbacks = [],
     stopListeningCallbacks = [],
-    ioF = return (),
     apiState = nullPtr
 }
 
-type ProgramLoggerM a = State ProgramLoggerS a
+newtype ProgramLoggerM a = ProgramLoggerM { runProgramLogger :: ProgramLoggerS -> IO (a, ProgramLoggerS) }
 
-cleanupProgramLogger :: ProgramLoggerS -> IO ()
+instance Monad ProgramLoggerM where
+        --return x = return $ \s -> (x, s)
+        (>>=) m f = \s -> do
+            (r, s') <- m s
+            f s'
+        {-(>>=) a f = \s -> do
+            (a', s') <- a s
+            (x, y)   <- f a'
+            y s'-}
+
+-- Adapted from the LlvmM monad in GHC
+-- see compiler/llvmGen/LlvmCodeGen/Base.hs
+instance Functor ProgramLoggerM where
+    fmap f m = ProgramLoggerM $ \s -> do 
+            (x, s') <- runProgramLogger m s
+            return (f x, s')
+
+instance Applicative ProgramLoggerM where
+    pure x = ProgramLoggerM $ \s -> return (x, s)
+    (<*>) = ap
+
+instance Monad ProgramLoggerM where
+    m >>= f  = LlvmM $ \s -> do (x, env') <- runProgramLoggerM m s 
+                                  runLlvmM (f x) env'
+
+
+
+getState :: ProgramLoggerM ProgramLoggerS
+getState s = return (s, s) 
+
+cleanupProgramLogger :: ProgramLoggerM ()
 cleanupProgramLogger s = do
         freeAPIState (apiState s)
         mapM_ freeHaskellFunPtr (eventCallbacks s)
         mapM_ freeHaskellFunPtr (errorCallbacks s)
         mapM_ freeHaskellFunPtr (stopListeningCallbacks s)
 
-runProgramLogger :: ProgramLoggerM a -> IO a
+{-runProgramLogger :: ProgramLoggerM a -> IO a
 runProgramLogger stateAction = do
         apiStatePtr <- initializeAPIState
 
@@ -39,3 +66,4 @@ runProgramLogger stateAction = do
 
         cleanupProgramLogger finalState
         return res
+        -}
