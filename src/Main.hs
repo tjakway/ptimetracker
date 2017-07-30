69ad0aadc252eebf978a8e8b85ef1cf0c01168ef
Thomas Jakway <tjakway@nyu.edu>
2017-07-30 15:36:59 -0400

    updated example EventCallback

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
 import Data.IORef
+import Foreign.C.String
 import TimeTracker.Interface
 import TimeTracker.Types
 
 
+-- TODO: shouldn't have to handle marshalling in the callback
 main :: IO ()
-main = let eventCallback pid _ = putStrLn ("PID called from Haskell: " ++ (show pid))
+main = let eventCallback pid _ name = peekCString name >>= (\n -> putStrLn ("PID called from Haskell: " ++ (show pid) ++ ", name: " ++ n))
            procRegex = ".*"
            cwdRegex = ".*"
            procM = addProcMatcher eventCallback procRegex False cwdRegex
