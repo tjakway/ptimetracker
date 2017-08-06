1d5cbdf8ec260860963892ea9553fd20f273689a
Thomas Jakway <tjakway@nyu.edu>
2017-08-06 12:15:15 -0400

    wrote createTables

--- src/TimeTracker/IO/Database.hs
+++ src/TimeTracker/IO/Database.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module TimeTracker.IO.Database where
 
 import Database.HDBC
 import Control.Monad.Reader
 
-data DBData = DBData String -- XXX
+data DBData = 
+    forall a . IConnection a => 
+        DBData {
+            connection :: a,
+            createTablesStmt :: Statement,
+            insertProcEventStmt :: Statement,
+            insertTickResolutionStmt :: Statement,
+            insertProcEventTypeStmt :: Statement
+        }
 
 type DBMonad a = ReaderT DBData IO a
 
+createTables :: DBMonad ()
+createTables = (createTablesStmt <$> ask) >>= liftIO . executeRaw
