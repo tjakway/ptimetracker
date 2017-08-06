0dda04af323eb2a9503ede996de9977b3719aef9
Thomas Jakway <tjakway@nyu.edu>
2017-08-06 12:19:30 -0400

    fixed type errors

--- src/TimeTracker/Config.hs
+++ src/TimeTracker/Config.hs
@@ -19,18 +19,14 @@ data Flag = EnableTicks
 -- XXX: TODO
 -- TODO: write configuration defaults
 flagsToConfig :: [Flag] -> Either String Config
-flags [] = Left "No flags passed."
+flagsToConfig [] = Left "No flags passed."
 
 options :: [OptDescr Flag]
 options = 
-        [ Option ['t'] ["enable-ticks"] 
-            (NoArg EnableTicks) "Enable periodic program logging.",
-          Option [] ["db-url"]
-            (ReqArg DbUrl "URL or filesystem path of the database to connect to."),
-          Option ['u'] ["username"]
-            (ReqArg DbUsername) "Username for the database, if applicable.",
-          Option ['p'] ["password"]
-            (ReqArg DbPassword) "Password for the database, if applicable.",
+        [ Option ['t'] ["enable-ticks"] (NoArg EnableTicks) "Enable periodic program logging.",
+          Option [] ["db-url"] (ReqArg DbUrl "URL") "URL or filesystem path of the database to connect to.",
+          Option ['u'] ["username"] (ReqArg DbUsername "USERNAME") "Username for the database, if applicable.",
+          Option ['p'] ["password"] (ReqArg DbPassword "PASSWORD") "Password for the database, if applicable."
             ]
 
 
