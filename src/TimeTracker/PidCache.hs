74318c5f9edb20cc5d8ee5bba007cf8d7310d96e
Thomas Jakway <tjakway@nyu.edu>
2017-08-14 15:35:27 -0400

    wrote PidCache, now exporting it

--- src/TimeTracker/PidCache.hs
+++ src/TimeTracker/PidCache.hs
@@ -1,4 +1,9 @@
-module TimeTracker.PidCache where
+module TimeTracker.PidCache 
+(
+initPidCache,
+withPidCache
+)
+where
 
 import Data.Map.Strict as Map
 import Data.IORef
@@ -10,17 +15,25 @@ type PidCache = Map.Map FFI.PidT String
 initPidCache :: IO (IORef PidCache)
 initPidCache = newIORef Map.empty
 
+-- | wraps the EventCallback and filters PROC_END events for which we don't
+-- know the program name
 withPidCache :: IORef PidCache -> EventCallback -> EventCallback
 withPidCache cache f = \pid eventCode progName ->
     let eventCode' = FFI.intToProcMatchEventType (fromInteger eventCode)
                                         -- unknown event type, skip the
                                         -- cache
         f' = f pid eventCode progName
+        pid' = fromInteger pid
         in case eventCode' of Nothing -> f'
                               -- only write to the cache on ProcStart
                               Just (FFI.ProcStart) -> do
-                                  let pid' = fromInteger pid
                                   modifyIORef' cache (insert pid' progName)
                                   f'
+                              Just (FFI.ProcEnd) -> do
+                                  hasPid <- Map.lookup pid' <$> readIORef cache
+                                  -- if we don't have the PID in the cache,
+                                  -- filter out the PROC_END event
+                                  case hasPid of Nothing -> return ()
+                                                 Just _  -> f'
                               _ -> f'
 
