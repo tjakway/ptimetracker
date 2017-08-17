fed76cfc3c18ec609242efc1f3a346fa451a497a
Thomas Jakway <tjakway@nyu.edu>
2017-08-16 18:21:27 -0700

    now depending on hslogger

--- src/TimeTracker/Config/ConfigTypes.hs
+++ src/TimeTracker/Config/ConfigTypes.hs
@@ -1,11 +1,13 @@
 module TimeTracker.Config.ConfigTypes where
 
+import System.Log.Logger
 
 data ConnectionInfo = Sqlite FilePath
                     --Postgres URL of the form https://www.postgresql.org/docs/8.1/static/libpq.html#LIBPQ-CONNECT
                     | Postgres String
 
 data Config = Config {
+              loggingPriority :: Priority,
               connectionInfo :: ConnectionInfo,
               ticksEnabled :: Bool
             }
