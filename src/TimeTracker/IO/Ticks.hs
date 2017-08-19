6fec504b6fc8d747ac5be5e681f2052aa6f20a50
Thomas Jakway <tjakway@nyu.edu>
2017-08-19 07:36:06 -0700

    work on recordTicks

--- src/TimeTracker/IO/Ticks.hs
+++ src/TimeTracker/IO/Ticks.hs
@@ -1,13 +1,22 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module TimeTracker.IO.Ticks where
 
+import System.Directory
+import Data.Map.Strict
 import TimeTracker.IO.Database
 import TimeTracker.PidCache
 
--- use bracketOnError from Control.Exception to rollback on exception
 
--- | eventId = the ID column that 
+procDirExists :: PidT -> IO Bool
+procDirExists = doesDirectoryExist . procDir
+    where procDir :: PidT -> FilePath
+          procDir = mappend "/proc/" . show
+
+isRunning :: PidCache -> PidT -> IO Bool
+isRunning cache pid = case procDirExists pid <$> lookup pid cache of Just x -> return x
+                                                                     Nothing -> return False
+
 recordTicks :: PidCache -> Int -> DbMonad ()
-recordTicks cache eventId = undefined
-
-
+recordTicks cache eventId = bracketOnErrorM_ (return ()) rollbackDb rec'
+    where rec' = do
 
